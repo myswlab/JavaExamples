718dc31c66fd604fdd519d84654eb46e5cbffb8f
Ivan Serina <ivan.serina@unibz.it>
2015-06-03 12:02:29 +0200

    titoli azionari added

---
+++ src/it/unibs/ing/fp/esercizi_laboratorio/titoli/Lotto.java
@@ -0,0 +1,27 @@
+package it.unibs.ing.fp.esercizi_laboratorio.titoli;
+
+import java.io.Serializable;
+
+public class Lotto implements Serializable
+{
+ private Titolo azione;
+ private int quantita;
+	
+ public Lotto (Titolo _azione, int _quantita)
+	{
+	 azione = _azione;
+	 quantita = _quantita;
+	}
+	
+ public double valore ()
+	{
+	 return quantita*azione.getPrezzo();
+	}
+	
+ public String toString ()
+	{
+	 return quantita + " " + azione.toString();
+	}
+
+}
+
---
+++ src/it/unibs/ing/fp/esercizi_laboratorio/titoli/Portafoglio.java
@@ -0,0 +1,67 @@
+package it.unibs.ing.fp.esercizi_laboratorio.titoli;
+
+import java.util.*;
+
+import java.io.Serializable;
+
+
+public class Portafoglio implements Serializable
+{
+ 
+ private final static String DESC_DENOMINAZIONE = "Portafoglio denominato: %s %n";
+ private final static String INTRO_COMPOSIZIONE = "Composizione:\n";
+ private final static String DESC_TOTALE = "Valore totale= %1.2f";
+ 
+ private String denominazione;
+ private ElencoTitoli listaTitoli;
+
+ private Vector<Lotto> composizione;
+ 
+ public Portafoglio (String _denominazione, ElencoTitoli _listaTitoli)
+	{
+	 denominazione = _denominazione;
+	 listaTitoli = _listaTitoli;
+	 composizione = new Vector<Lotto>();
+	}
+
+ public void addLotto(Lotto l)
+	{
+	 composizione.add(l);
+	}
+	
+ public double valore()
+	{
+	 double totale = 0;
+	 for (int i = 0; i < composizione.size(); i++)
+		{
+		 Lotto elem = (Lotto) composizione.get(i);
+		 totale = totale + elem.valore();
+		}
+		
+	 return totale;
+	}
+	
+	public void simulaVariazione ()
+	 {
+		listaTitoli.simulaVariazione();
+	 }
+ 
+  public String toString()
+	 {
+		 StringBuffer result = new StringBuffer();
+			
+		 result.append (String.format(DESC_DENOMINAZIONE, denominazione));
+		 result.append (INTRO_COMPOSIZIONE);
+
+		 for (int i = 0; i < composizione.size(); i++)
+			{
+			 result.append( composizione.get(i).toString() + "\n");
+			}
+			
+		 result.append(String.format(DESC_TOTALE, valore()));
+		 
+		 return result.toString();
+	 }
+
+}
+
