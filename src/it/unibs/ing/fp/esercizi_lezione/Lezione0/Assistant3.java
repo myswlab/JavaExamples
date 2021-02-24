4f545e909392eabe5fd22ac05c5da069f5ff0f54
Ivan <ivan.serina@unibs.it>
2021-02-24 11:04:35 +0100

    Assistant3

--- src/it/unibs/ing/fp/esercizi_lezione/Lezione0/Assistant3.java
+++ src/it/unibs/ing/fp/esercizi_lezione/Lezione0/Assistant3.java
@@ -1,1 +1,1 @@
-public class Assistant2{private String mioSaluto;public Assistant2(){mioSaluto = "Ciao";}public Assistant2(String cosaDevoDire){mioSaluto=cosaDevoDire;}public void saluta(){System.out.println(mioSaluto);}} 
+public class Assistant3{private String mioSaluto;public Assistant3(){mioSaluto = "Ciao";}public Assistant3(String cosaDevoDire){mioSaluto=cosaDevoDire;}public void saluta(){System.out.println(mioSaluto);}} 
--- src/it/unibs/ing/fp/schildt/ch5Iterators/es11ForTick/ForTick.java
+++ src/it/unibs/ing/fp/schildt/ch5Iterators/es11ForTick/ForTick.java
@@ -5,7 +5,7 @@ class ForTick {
   public static void main(String args[]) {
 
     // here, n is declared inside of the for loop
-    for(int n=10; n>0; n--)
+    for(int n=10; n>0; n --)
       System.out.println("tick " + n);
   }
 }
